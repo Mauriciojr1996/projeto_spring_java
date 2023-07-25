fb1534f9e6d110cf06bfdb84044a88db7f143f5a
Mauricio cavalcanti junior <mauricio.cavalcanti@inmetrics.com.br>
2023-07-25 16:06:56 -0300

    versão 2 adicionando departamentos de administração, Recursos humanos e Financeiro.

--- userdept/src/main/resources/import.sql
+++ userdept/src/main/resources/import.sql
@@ -1,7 +1,21 @@
 INSERT INTO tb_department(name) VALUES ('Gestão');
 INSERT INTO tb_department(name) VALUES ('Informática');
+INSERT INTO tb_department(name) VALUES ('Administração');
+INSERT INTO tb_department(name) VALUES ('Recursos humanos');
+INSERT INTO tb_department(name) VALUES ('Financeiro');
 
 INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (3, 'Kaique', 'Kaique@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (3, 'Jaqueline', 'Jaqueline@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (3, 'Jorge', 'Jorge@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (4, 'Kimberly', 'Kimberly@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (4, 'Gabriel', 'Gabriel@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (5, 'Alberto', 'Alberto@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (5, 'Frances', 'Frances@gmail.com');
+
+
+
+
